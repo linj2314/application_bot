e536297f6d8b74ea5d2daf8f7c20e25f93f525ce
linj2314 <linj2314@gmail.com>
2024-05-19 18:06:41 -0400

    greenhouse.py done for now may need reforms later

--- greenhouse.py
+++ greenhouse.py
@@ -11,7 +11,7 @@ driver.implicitly_wait(2)
 driver.get("https://boards.greenhouse.io/torcrobotics/jobs/7067416002")
 
 text_inputs = driver.find_elements(By.CSS_SELECTOR, "input[type='text']:not([class])")
-dropdown_inputs_1 = driver.find_elements(By.CSS_SELECTOR, "div[class='select2-container']")
+dropdown_inputs_1 = driver.find_elements(By.CSS_SELECTOR, "div.select2-container")
 dropdown_inputs_2 = driver.find_elements(By.TAG_NAME, 'select')
 file_inputs = driver.find_elements(By.CSS_SELECTOR, "button[data-source='attach']")
 checkbox_questions = driver.find_elements(By.CSS_SELECTOR, "label label:nth-of-type(1) input[type='checkbox']")
@@ -20,22 +20,26 @@ skip_dropdown_2 = False
 if len(dropdown_inputs_1) > 0:
     skip_dropdown_2 = True
 
+try:
+    driver.find_element(By.CSS_SELECTOR, "input[aria-label='Education Start Month']").send_keys(answers["start_month"])
+    driver.find_element(By.CSS_SELECTOR, "input[aria-label='Education Start Year']").send_keys(answers["start_year"])
+    driver.find_element(By.CSS_SELECTOR, "input[aria-label='Education End Month']").send_keys(answers["end_month"])
+    driver.find_element(By.CSS_SELECTOR, "input[aria-label='Education End Year']").send_keys(answers["end_year"])
+except:
+    pass
+
 company_name = driver.find_element(By.CSS_SELECTOR, "span[class='company-name']").text
 company_name = company_name.split()
 company_name = company_name[1]
 role_name = driver.find_element(By.CSS_SELECTOR, "h1[class='app-title']").text
 
 for t in text_inputs:
-    if t.get_attribute("aria-label") == "Education Start Month":
-        t.send_keys(answers["start_month"])
-    elif t.get_attribute("aria-label") == "Education Start Year":
-        t.send_keys(answers["start_year"])
-    elif t.get_attribute("aria-label") == "Education End Month":
-        t.send_keys(answers["end_month"])
-    elif t.get_attribute("aria-label") == "Education End Year":
-        t.send_keys(answers["end_year"])
-    elif t.get_attribute("name") == "job_application[location]":
+    if t.get_attribute("name") == "job_application[location]":
         t.send_keys(answers["location"])
+        time.sleep(0.5)
+        t.send_keys(Keys.DOWN)
+        t.send_keys(Keys.ENTER)
+        continue
     prompt = t.find_element(By.XPATH, '..').text
     if not prompt:
         continue
@@ -51,46 +55,23 @@ for t in text_inputs:
             pass
 
 for d in dropdown_inputs_1:
-    prompt = d.find_element(By.XPATH, '..').text
+    parent = d.find_element(By.XPATH, '..')
+    prompt = parent.text
     prompt = prompt.split('\n')[0]
-    print(prompt)
+    if not prompt:
+        continue
     response = Response(prompt)
     if response == 'skip_fs':
         continue
-    d.click()
-    input = driver.find_element(By.CSS_SELECTOR, 'div[id="select2-drop"] div input')
-    options = driver.find_elements(By.CSS_SELECTOR, 'div[id="select2-drop"] ul li div')
-    if response == "skip":
-        choices = []
-        for o in options:
-            choices.append(o.text)
-        submission = AI(prompt, choices)
-        down = 0
-        for o in options:
-            if o.text == submission:
-                break
-            down += 1
-        for _ in range(down):
-            input.send_keys(Keys.DOWN)
-        input.send_keys(Keys.ENTER)
-    else:
-        choices = []
-        submission = answers[response]
-        down = 0
-        found = False
-        for o in options:
-            choice = o.text
-            choices.append(choice)
-            choice = clean_str(choice)
-            if choice == submission:
-                found = True
-                break
-            down += 1
-        if found:
-            for _ in range(down):
-                input.send_keys(Keys.DOWN)
-            input.send_keys(Keys.ENTER)
-        else:
+    try:
+        parent.find_element(By.CSS_SELECTOR, "select")
+        d.click()
+        input = driver.find_element(By.CSS_SELECTOR, 'div[id="select2-drop"] div input')
+        options = driver.find_elements(By.CSS_SELECTOR, 'div[id="select2-drop"] ul li div')
+        if response == "skip":
+            choices = []
+            for o in options:
+                choices.append(o.text)
             submission = AI(prompt, choices)
             down = 0
             for o in options:
@@ -100,6 +81,44 @@ for d in dropdown_inputs_1:
             for _ in range(down):
                 input.send_keys(Keys.DOWN)
             input.send_keys(Keys.ENTER)
+        else:
+            choices = []
+            submission = answers[response]
+            down = 0
+            found = False
+            for o in options:
+                choice = o.text
+                choices.append(choice)
+                choice = clean_str(choice)
+                if choice == submission:
+                    found = True
+                    break
+                down += 1
+            if found:
+                for _ in range(down):
+                    input.send_keys(Keys.DOWN)
+                input.send_keys(Keys.ENTER)
+            else:
+                submission = AI(prompt, choices)
+                down = 0
+                for o in options:
+                    if o.text == submission:
+                        break
+                    down += 1
+                for _ in range(down):
+                    input.send_keys(Keys.DOWN)
+                input.send_keys(Keys.ENTER)
+    except:
+        if response == "skip":
+            submission = AI(prompt)
+        else:
+            submission = answers[response]
+        d.click()
+        input = driver.find_element(By.CSS_SELECTOR, 'div[id="select2-drop"] div input')
+        input.send_keys(submission)
+        time.sleep(0.5)
+        input.send_keys(Keys.ENTER)
+    
 
 if not skip_dropdown_2:
     for d in dropdown_inputs_2:
@@ -142,7 +161,6 @@ for c in checkbox_questions:
         for b in boxes:
             choices.append(b.text)
         submission = AI(prompt, choices)
-        print(submission)
         for b in boxes:
             if b.text.strip() == submission:
                 b.find_element(By.CSS_SELECTOR, "input").click()
@@ -180,8 +198,8 @@ for f in file_inputs:
     time.sleep(0.5)
     subprocess.Popen(['xdotool', 'key', 'Return'])
     time.sleep(1)
-
-#driver.find_element(By.ID, 'submit_app').click()
+    
+driver.find_element(By.ID, 'submit_app').click()
 
 time.sleep(100)
 
